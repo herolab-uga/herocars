cf9828c540dc207c28b6ea109e2e8f162abdf24c
Michael Starks <starksmichael224@gmail.com>
2022-05-19 18:28:07 -0400

    stopped program close on disconnect

--- AI_Driver/Socket/Client.py
+++ AI_Driver/Socket/Client.py
@@ -27,49 +27,54 @@ def TCP (car):
     if (port > 65535 or port < 1024):
       port = 1024 + carNum
     print(port)
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    ip = GetServerIP(carNum)
-    print(ip)
-    server_address = (ip, port)
-    print(server_address)
-    try:
-        sock.connect(server_address)
-    except Exception as e:
-        print(e)
-        os._exit(0)
-    print("here")
-    sock.settimeout(3)
-    isConnected = True
-    car.isConnected = True
-    print("Connected")
-    while isConnected:
+    while True:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        ip = GetServerIP(carNum)
+        print(ip)
+        server_address = (ip, port)
+        print(server_address)
         try:
-            data = sock.recv(11, socket.MSG_WAITALL)
-        except socket.timeout as e:
-            car.isConnected = False
-            isConnected = False
-            print("closed")
-            sock.close()
+            sock.connect(server_address)
+        except Exception as e:
+            print(e)
             os._exit(0)
-        if not data: break
-        car.modifyPID(data)
-        try:
-            speed = car.speed
-            if (speed < 0):
-                speed = 0
-            elif (speed > 255):
-                speed = 255
-            rList = [car.error + 10, speed]
-            arr = bytearray(rList)
-            sock.sendall(arr)
-        except BrokenPipeError as e:
-            car.isConnected = False
-            isConnected = False
-            print("closed")
-            sock.close()
-            os._exit(0)
-    print("exit")
-    os._exit(0)
+        print("here")
+        sock.settimeout(5)
+        isConnected = True
+        car.isConnected = True
+        print("Connected")
+        while isConnected:
+
+            try:
+                data = sock.recv(11, socket.MSG_WAITALL)
+            except socket.timeout as e:
+                car.isConnected = False
+                isConnected = False
+                print("closed")
+                sock.close()
+            if not data: 
+                break
+
+            car.modifyPID(data)
+
+            try:
+                speed = car.speed
+                if (speed < 0):
+                    speed = 0
+                elif (speed > 255):
+                    speed = 255
+                rList = [car.error + 10, speed]
+                arr = bytearray(rList)
+                sock.sendall(arr)
+
+            except BrokenPipeError as e:
+                car.isConnected = False
+                isConnected = False
+                print("closed")
+                sock.close()
+                os._exit(0)
+        print("Looking")
+
 def GetServerIP(deviceNumber):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     if (int(deviceNumber) > 9):
