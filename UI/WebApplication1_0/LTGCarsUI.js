bb661b2566b63525259f0c4a6a57d176323c375f
miasavelli <miasavelli@uga.edu>
2022-07-01 20:12:37 -0400

    UI Additions

--- UI/WebApplication1_0/LTGCarsUI.js
+++ UI/WebApplication1_0/LTGCarsUI.js
@@ -1,61 +1,70 @@
 // https://blog.idrisolubisi.com/how-to-integrate-real-time-webcam-on-a-web-page-using-javascript
-// sliders for p, i, d
-// sliders for min and max
+
 // big camera view
 // button to switch control modes
 // button to switch line types
 
-function startLTGCars() {
-    // ip address and stuff and connect button
-    LTGCarsArea.start();
-}
+var p = document.getElementById("pRange");
+var i = document.getElementById("iRange");
+var d = document.getElementById("dRange");
+var min = document.getElementById("min");
+var max = document.getElementById("max");
 
-var LTGCarsArea = { // to canvas or no canvas 
-    canvas : document.createElement("canvas"),
-    start : function() {
-        this.canvas.width = 600;
-        this.canvas.height = 300;
-        this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.frameNo = 0;
-        // this.interval = setInterval(updateCarImg, 20); update to socket, probably not 20 seconds
-        },
-    clear : function() {
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+var pValToPrint = document.getElementById("pPrint"); // to show user
+var iValToPrint = document.getElementById("iPrint");
+var dValToPrint = document.getElementById("dPrint");
+var minToPrint = document.getElementById("min");
+var maxToPrint = document.getElementById("max");
+
+function start() {
+    if (enableButtons( ) == true) {
+        console.log("Program Started");
+        setInterval(updateCarIMG(), 1000);
+    } else {
+        // disable buttons
     }
+    
 }
 
 var Movement = {
     right: function() {
-
+        // send message to car to move 
+        console.log("Moving right");
     },
     left: function() {
-
+        console.log("Moving left");
     },
     forward: function() {
-
+        console.log("Moving forward");
     },
     backward: function() {
-        
+        console.log("Moving backward");
     }
 }
 
 function updateCarIMG () {
     // just something to update the image received from the car
+    console.log("Image updated");
 }
 
-function updateCarPlacement() {
+// updates car's placement from up and down arrow keys & buttons on webpage
+function updateCar() {
 
     node.addEventListener('keydown', function(event) {
-        const key = event.key; // "ArrowRight", "ArrowLeft", "ArrowUp", or "ArrowDown"
+        const key = event.key;
         if (key == "ArrowRight")
             Movement.right();
         if (key == "ArrowLeft")
             Movement.left();
+        if (key == "ArrowUp")
+            Movement.forward();
+        if (key == "ArrowDown")
+            Movement.backward();
     });
 }
 
-function disableButtons() {
+function enableButtons() {
+    return true;
     // disable buttons if connection is lost.
     // check for enabling.
 }
