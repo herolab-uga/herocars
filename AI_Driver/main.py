a84a2defe379e252607d9ab6f4b6d586f3733173
Michael Starks <starksmichael224@gmail.com>
2022-07-02 18:04:22 -0400

    manual control test

--- AI_Driver/main.py
+++ AI_Driver/main.py
@@ -1,1 +1,24 @@
+import getch
+import threading
+import Controllers.CarController as CarController
+
+
+def main():
+    
+    car = CarController()
+
+    while True:
+        ctl = getch.getch()
+        if ctl == 'w':
+            car.forward()
+        elif ctl == 's':
+            car.backward()
+        elif ctl == 'a':
+            car.left()
+        elif ctl == 'd':
+            car.right()
+
+if __name__ == '__main__':
+    main()
+
 
