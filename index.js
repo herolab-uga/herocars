88f5ac67a8f5d5a12afe21108dd0ab69aa00f17e
miasavelli <miasavelli@uga.edu>
2022-07-03 13:27:31 -0400

    changed to createConnection

--- index.js
+++ index.js
@@ -33,13 +33,6 @@ const port_control = 5000;
 const client = Net.createConnection({ port: port_control }, () => {
     // 'connect' listener.
     console.log('connected to server!');
-    client.write('world!\r\n');
-  });
-
-client.connect({ port: port_control, host: host }), function() {
-    console.log('TCP connection established with the server.');
-
-
     setInterval(() => {
         updateAll();
     }, 1000);
@@ -77,10 +70,50 @@ client.connect({ port: port_control, host: host }), function() {
             client.write(lineType)
         }
     }
-    
-    
+  });
 
-};
+// client.connect({ port: port_control, host: host }), function() {
+//     console.log('TCP connection established with the server.');
+
+
+//     setInterval(() => {
+//         updateAll();
+//     }, 1000);
+    
+//     function updateAll() {
+//         if (p != ltgcars.getP()) {
+//             p = ltgcars.getP();
+            
+//             client.write(1);
+//             client.write(p);
+//         }
+//         if (i != ltgcars.getI()) {
+//             i = ltgcars.getI();
+//             client.write(2);
+//             client.write(i);
+//         }
+//         if (d != ltgcars.getD()) {
+//             d = ltgcars.getD();
+//             client.write(3);
+//             client.write(d);
+//         }
+//         if (minSpeed != ltgcars.getMinSpeed()) {
+//             minSpeed = ltgcars.getMinSpeed();
+//             client.write(5);
+//             client.write(minSpeed);
+//         }
+//         if (maxSpeed != ltgcars.getMaxSpeed()) {
+//             maxSpeed = ltgcars.getMaxSpeed();
+//             client.write(6);
+//             client.write(maxSpeed);
+//         }
+//         if (lineType != ltgcars.getLineType()) { // white is true
+//             lineType = ltgcars.getLineType();
+//             client.write(8);
+//             client.write(lineType)
+//         }
+//     }
+// };
 
 // The client can also receive data from the server by reading from its socket.
 client.on('data', function(chunk) {
