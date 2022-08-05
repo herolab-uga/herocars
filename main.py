82b9829e4e8f73f4d9e2ecac08ea852a382a1393
Michael Starks <starksmichael224@gmail.com>
2022-08-04 20:46:18 -0400

    debugging

--- main.py
+++ main.py
@@ -122,7 +122,7 @@ def camera_feed():
 
 @app.route("/irsensors",methods=["GET"])
 def irsensors():
-    print(request.form["fname"])
+    print(request.form.keys())
     return render_template('index.html')
 
 @app.route("/")
