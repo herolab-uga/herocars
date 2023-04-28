d47fc9eb809291fa587be847bd29a4a848e74dbb
Michael Starks <starksmichael224@gmail.com>
2023-04-28 12:31:18 -0400

    debugging

--- AI_Driver/Controllers/CarController.py
+++ AI_Driver/Controllers/CarController.py
@@ -63,7 +63,7 @@ class CarController:
 
     # Gets the speed proportional to the _error
     def calculate_speed(self):
-        self._car_speed = min(abs(int(abs(self._error) * (self._max_speed /4))) + self._min_speed, self._max_speed)
+        self._car_speed = min(abs(int(abs(self._error) * (self.speed["max"] /4))) + self.speed["min"], self.speed["max"])
         return self._car_speed
 
     # Reads the frame from the camera
@@ -101,7 +101,7 @@ class CarController:
         line_state = [GPIO.input(37), GPIO.input(35), GPIO.input(33), GPIO.input(31), GPIO.input(29)]
 
         # If the line is set to white return the list
-        if self._line_color == "White":
+        if self.control_variables["line_color"] == "White":
             return line_state
         # If the line is set to black return the list with the opposite values
         else:
@@ -109,39 +109,39 @@ class CarController:
 
     def calculate_error(self):
 
-        self.prevError = self._error
+        self.pid_values["prev_error"] = self.pid_values["error"]
 
         # Combine IR sensors into _error value
 
         # Get the states of the IR sensors
         LL,LM,MM,RM,RR = self.get_line_state()
         try:
-            self._error = (4*RR + 2*RM + 0 + -2*LM + -4*LL) / (RR + RM + MM + LM + LL)
+            self.pid_values["error"] = (4*RR + 2*RM + 0 + -2*LM + -4*LL) / (RR + RM + MM + LM + LL)
         except ZeroDivisionError:
             print("not on line")
-        if abs(self._error) < 4:
-            self._PV += -.0001 * self._error
+        if abs(self.pid_values["error"]) < 4:
+            self.pid_values["PV"] += -.0001 * self.pid_values["error"]
 
-        self._PV = max(self._PV,-10)
-        self._PV = min(self._PV,10)
+        self.pid_values["PV"] = max(self.pid_values["PV"],-10)
+        self.pid_values["PV"] = min(self.pid_values["PV"],10)
             
-        return self._error
+        return self.pid_values["error"]
 
     def auton_control_update(self):
         while True:
-            if self._control_type == 1:
+            if self.control_variables["control_type"] == 1:
                 _error = self.calculate_error()
-                correction = self._p * _error + self._i * self._PV + self._d * (self._error -self._prev_error)
+                correction = self.pid_values["p"] * _error + self.pid_values["i"] * self.pid_values["PV"] + self.pid_values["d"] * (self.pid_values["error"] -self.pid_values["prev_error"])
                 self._motor_driver.Turn(correction)
                 self._motor_driver.Drive(self.calculate_speed())
 
     def car_auto_stop(self):
         while True:
-            if self._control_type == 0: 
-                if time.time() - self._last_velo_time > .05 and not self._car_speed == 0 :
+            if self.control_variables["control_type"] == 0: 
+                if time.time() - self.last_times["last_velo_time"] > .05 and not self.speed["current_speed"] == 0 :
                     self.stop()
-                if time.time() - self._last_steer_time > .05 and not self._straight:
-                    self._straight = 1
+                if time.time() - self.last_times["last_steer_time"] > .05 and not self.control_variables["straight"]:
+                    self.control_variables["straight"] = 1
                     self.center_steering()
             time.sleep(.01)
 
